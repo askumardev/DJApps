1c48e268fd1cfb87125953af052b49fb87286f6f
askumardev <askumar.dev@outlook.com>
2022-07-06 12:47:58 +0530

    new update

--- djApp/djApp/settings.py
+++ djApp/djApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-il-dsj)-gyn5kf^$)j8qvmyt8z*ak!$dcr10xk72-m3k&iocrj'
+
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
